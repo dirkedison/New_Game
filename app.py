8c66ef88d99aafe500bd10ccd60037932ccb2a3d
dirkedison <dirk.edison@gmail.com>
2024-06-27 12:00:57 -0400

    more leveling fixes

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ if 'woodcutting' not in st.session_state:
     st.session_state.woodcutting = 1
 
 def level_up(skill):
-    if st.session_state[skill] < 100:
-        st.session_state[skill] += 1
+    st.session_state[skill] += 1
 
 st.title("Simple RPG Game")
 
@@ -16,22 +15,22 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.subheader("Mining")
-    st.write(f"Current level: {st.session_state.mining}")
     if st.button("Mine", key="mine"):
         level_up('mining')
+    st.write(f"Current level: {st.session_state.mining}")
     
-    if st.session_state.mining < 9:
+    if st.session_state.mining < 10:
         st.write("You can mine copper ore.")
     else:
         st.write("You can mine copper and silver ore.")
 
 with col2:
     st.subheader("Woodcutting")
-    st.write(f"Current level: {st.session_state.woodcutting}")
     if st.button("Chop", key="chop"):
         level_up('woodcutting')
+    st.write(f"Current level: {st.session_state.woodcutting}")
     
-    if st.session_state.woodcutting < 9:
+    if st.session_state.woodcutting < 10:
         st.write("You can chop normal trees.")
     else:
         st.write("You can chop normal and oak trees.")
