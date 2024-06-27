f2336129f6a3a8db149100cb726910e2b7e7af77
dirkedison <dirk.edison@gmail.com>
2024-06-27 12:09:47 -0400

    added different buttons for different items

--- app.py
+++ app.py
@@ -9,32 +9,38 @@ if 'woodcutting' not in st.session_state:
 def level_up(skill):
     st.session_state[skill] += 1
 
-st.title("Best RPG Game")
+st.title("Simple RPG Game")
 
 col1, col2 = st.columns(2)
 
 with col1:
     st.subheader("Mining")
-    if st.button("Mine", key="mine"):
-        level_up('mining')
     st.write(f"Current level: {st.session_state.mining}")
     
-    if st.session_state.mining < 5:
+    if st.button("Mine Copper", key="mine_copper"):
+        level_up('mining')
+    
+    if st.session_state.mining >= 10:
+        if st.button("Mine Silver", key="mine_silver"):
+            level_up('mining')
+    
+    if st.session_state.mining < 10:
         st.write("You can mine copper ore.")
     else:
-        st.write("You can mine copper ore.")
-        st.write("AND")
-        st.write("You can mine silver ore.")
+        st.write("You can mine copper and silver ore.")
 
 with col2:
     st.subheader("Woodcutting")
-    if st.button("Chop", key="chop"):
-        level_up('woodcutting')
     st.write(f"Current level: {st.session_state.woodcutting}")
     
-    if st.session_state.woodcutting < 5:
+    if st.button("Chop Normal Trees", key="chop_normal"):
+        level_up('woodcutting')
+    
+    if st.session_state.woodcutting >= 10:
+        if st.button("Chop Oak Trees", key="chop_oak"):
+            level_up('woodcutting')
+    
+    if st.session_state.woodcutting < 10:
         st.write("You can chop normal trees.")
     else:
-        st.write("You can chop normal trees.")
-        st.write("AND")
-        st.write("You can chop oak trees.")
+        st.write("You can chop normal and oak trees.")
