de5a6637b79dd538f71a8e1816bcf2e4f473d970
dirkedison <dirk.edison@gmail.com>
2024-06-28 09:16:35 -0400

    introduced blacksmithing

--- app.py
+++ app.py
@@ -5,12 +5,24 @@ if 'state' not in st.session_state:
     st.session_state.state = {
         'mining': 1,
         'woodcutting': 1,
-        'inventory': {'copper': 0, 'silver': 0, 'wood': 0, 'oak_wood': 0}
+        'blacksmithing': 1,
+        'inventory': {'copper': 0, 'silver': 0, 'wood': 0, 'oak_wood': 0, 'copper_sword': 0, 'silver_sword': 0},
+        'show_blacksmith_unlock': False
     }
 
 def level_up_and_gather(skill, item):
     st.session_state.state[skill] += 1
     st.session_state.state['inventory'][item] += 1
+    if skill == 'mining' and st.session_state.state[skill] == 10:
+        st.session_state.state['show_blacksmith_unlock'] = True
+
+def craft_item(item, resource):
+    if st.session_state.state['inventory'][resource] > 0:
+        st.session_state.state['inventory'][resource] -= 1
+        st.session_state.state['inventory'][item] += 1
+        st.session_state.state['blacksmithing'] += 1
+        return True
+    return False
 
 st.title("Simple RPG Game")
 
@@ -48,6 +60,27 @@ with col2:
     else:
         st.write("You can chop normal and oak trees.")
 
+if st.session_state.state['mining'] >= 10:
+    st.subheader("Blacksmithing")
+    st.write(f"Current level: {st.session_state.state['blacksmithing']}")
+    
+    if st.session_state.state['show_blacksmith_unlock']:
+        st.success("You unlocked blacksmithing!")
+        st.session_state.state['show_blacksmith_unlock'] = False
+    
+    if st.button("Make Copper Sword", key="make_copper_sword", disabled=st.session_state.state['inventory']['copper'] == 0):
+        if craft_item('copper_sword', 'copper'):
+            st.success("Crafted a Copper Sword!")
+        else:
+            st.error("Not enough copper!")
+    
+    if st.session_state.state['blacksmithing'] > 9:
+        if st.button("Make Silver Sword", key="make_silver_sword", disabled=st.session_state.state['inventory']['silver'] == 0):
+            if craft_item('silver_sword', 'silver'):
+                st.success("Crafted a Silver Sword!")
+            else:
+                st.error("Not enough silver!")
+
 st.subheader("Inventory")
 for item, amount in st.session_state.state['inventory'].items():
-    st.write(f"{item.capitalize()}: {amount}")
+    st.write(f"{item.capitalize().replace('_', ' ')}: {amount}")
